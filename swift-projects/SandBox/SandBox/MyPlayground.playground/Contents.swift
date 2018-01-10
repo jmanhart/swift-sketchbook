b29223ae1ab104838c40953302e3f77c36267ada
Manhart, John <manhartjohn@gmail.com>
2018-01-10 10:58:32 -0600

    Adding in work with a playground and using cartography

--- swift-projects/SandBox/SandBox/MyPlayground.playground/Contents.swift
+++ swift-projects/SandBox/SandBox/MyPlayground.playground/Contents.swift
@@ -9,18 +9,37 @@ class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .gray
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 200)
-        label.text = "Hello World!"
-        label.textColor = .black
         
-        constrain(label){ label in
-            label.height == 20
+        /*
+        let boxWrapper = UIView()
+        boxWrapper.frame = CGRect(x: 40, y:0, width: 300, height: 500)
+        boxWrapper.backgroundColor = .red
+        */
+        
+        let box1 = UIView()
+        box1.frame = CGRect(x: 100, y: 200, width: 100, height: 100)
+        box1.backgroundColor = .blue
+        
+        let box2 = UIView()
+        box2.frame = CGRect(x: 200, y:200, width: 100, height: 100)
+        box2.backgroundColor = .green
+        
+        /* constrain(boxWrapper) { boxWrapper in
+            boxWrapper.width == boxWrapper.self.width
+            boxWrapper.height == boxWrapper.self.height
+            
+        }*/
+        
+        constrain(box1, box2) { box1, box2 in
+            box1.width == (box1.superview!.width - 100) * 0.5
+            box2.width == box1.width - 100
             
         }
+
         
-        view.addSubview(label)
+        
+        view.addSubview(box1)
+        view.addSubview(box2)
         self.view = view
     }
 }
