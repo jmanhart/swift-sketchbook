199914ccbbc2fb5f38edffcbc733512235ffe996
Manhart, John <manhartjohn@gmail.com>
2018-01-10 19:59:42 -0600

    like seriously what is the dif between CGSize and CGRect.  :\

--- swift-projects/SandBox/SandBox/MyPlayground.playground/Contents.swift
+++ swift-projects/SandBox/SandBox/MyPlayground.playground/Contents.swift
@@ -15,6 +15,7 @@ class MyViewController : UIViewController {
         
         // CardView Styling
         cardView.frame = CGRect(x: 100, y:100, width: 100, height: 100)
+        // cardView.frame = CGSize(width: 100, height: 100)
         cardView.backgroundColor = .white
         
         // Adding Subviews
@@ -27,14 +28,32 @@ class MyViewController : UIViewController {
             _ = make?.top.equalTo()(view.mas_top)?.offset()(10)
         }
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(cardViewTapped))
+        cardView.addGestureRecognizer(tap)
+        cardView.isUserInteractionEnabled = true
+        
         self.view = view
     }
     
     
-    
+    @objc func cardViewTapped () {
+        let animator = UIViewPropertyAnimator(duration: 0.7, dampingRatio: 0.7) {
+            // self.cardView.frame = CGRect(x: 100, y:100, width: 100, height: 100)
+            self.cardView.backgroundColor = .blue
+        }
+        animator.startAnimation()
+    }
 }
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()
+
+
+
+
+
+
+
+
 
 
 /*
